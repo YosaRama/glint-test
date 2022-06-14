9a1216e87600f8207300ba66656e9f4abd819c85
Yosa Rama <yosamelody07@gmail.com>
2022-06-14 10:17:53 +0800

    ADD: Loading behaviour

--- components/contents/homepage/index.js
+++ components/contents/homepage/index.js
@@ -1,4 +1,4 @@
-import { Col, Row } from "antd";
+import { Col, Row, Spin } from "antd";
 import MainCardPeopleList from "components/libs/card-people-list";
 import { useUsers } from "hooks/user";
 
@@ -7,7 +7,7 @@ import s from "./index.module.scss";
 
 function MainContentsHomepage() {
   //? ============== User Hook ============= ?//
-  const { data: userData } = useUsers({ queryString: "" });
+  const { data: userData, loading } = useUsers({ queryString: "" });
   // * ====================================== * //
 
   return (
@@ -16,23 +16,25 @@ function MainContentsHomepage() {
         <h1>Glints People Here</h1>
       </Col>
       <Row gutter={[16, 16]}>
-        {userData &&
-          userData?.map((item, index) => {
-            return (
-              <Col span={8} key={index}>
-                <MainCardPeopleList
-                  name={item.name}
-                  age={item.age}
-                  profile={
-                    item.profile_img
-                      ? item.profile_img
-                      : "https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg"
-                  }
-                  status={item.is_public}
-                />
-              </Col>
-            );
-          })}
+        <Spin spinning={loading}>
+          {userData &&
+            userData?.map((item, index) => {
+              return (
+                <Col span={8} key={index}>
+                  <MainCardPeopleList
+                    name={item.name}
+                    age={item.age}
+                    profile={
+                      item.profile_img
+                        ? item.profile_img
+                        : "https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg"
+                    }
+                    status={item.is_public}
+                  />
+                </Col>
+              );
+            })}
+        </Spin>
       </Row>
     </>
   );
